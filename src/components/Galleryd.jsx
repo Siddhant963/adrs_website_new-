867e0486b78c63c42d5a00a18ea4654e79cafc55
sapeksh <sapekshvishwakarma@gmail.com>
2025-10-26 17:20:45 +0530

    add images

--- src/components/Galleryd.jsx
+++ src/components/Galleryd.jsx
@@ -1,7 +1,8 @@
 const Gallery = () => {
   const images = [
     'p1.jpg', 'p2.jpg', 'p3.jpg', 'p4.jpg',
-    'p5.jpg', 'p6.jpg', 'p7.jpg', 'p1.jpg'
+    'p5.jpg', 'p6.jpg', 'p7.jpg', 'p1.jpg',
+    "p8.jpg", "p9.jpg", "p10.jpg"
   ];
 
   return (
